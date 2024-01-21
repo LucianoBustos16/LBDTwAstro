2135159cb06a30ae4231116cdf967e694a902b14
Luciano Bustos <bustosluciano89@gmail.com>
2024-01-21 00:51:24 -0300

    Add preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://lbdtw-astro.vercel.app/"
+  site: "https://lbdtw-astro.vercel.app/",
+  integrations: [preact()]
 });
